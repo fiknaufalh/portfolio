07dba211dab28ad43ca3f7bb970013a97854f507
fikrinaufal-paper <fikri.hamdi@paper.id>
2025-06-23 21:57:54 +0700

    chore: update resume

--- src/data/bio.ts
+++ src/data/bio.ts
@@ -10,7 +10,7 @@ export const Bio = {
       "I'm driven and adaptable, constantly seeking out fresh hurdles to conquer. Fueled by a love for acquiring new skills, I commit myself to producing top-notch outcomes. Armed with optimism and a commitment to growth, I stand prepared to add value and accomplish remarkable feats.",
     github: "https://github.com/fiknaufalh",
     resume:
-      "https://drive.google.com/file/d/13NmvmYWhJ-bATHUSxYMlm9IMzeGHJ6au/view?usp=sharing",
+      "https://drive.google.com/file/d/1RKBzdD-HB3_gilZZo64OSe-7MZWH3TW9/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/fiknaufalh/",
     twitter: "https://twitter.com/imnecker",
     instagram: "https://www.instagram.com/fiknaufal_",
